4cee1a1489970ecf8d7e8ee9dc619aa8628cf528
ssouvikk <souvik.edu.mondal@gmail.com>
2025-02-09 02:50:27 +0530

    docker & set up is done

--- server/Config.js
+++ server/Config.js
@@ -2,5 +2,4 @@ require('dotenv').config();
 
 module.exports = {
     FRONT_URL: process.env.FRONT_URL || 'http://localhost:3000',
-    DB_URL: process.env.DB_URL || "mongodb://localhost:27017/temperatureDB2",
 }
---
+++ server/index.js
@@ -0,0 +1,1 @@
+console.log('Server is ON for DB!')
